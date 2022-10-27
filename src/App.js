16ee1ff38ee7d6ec2078e7e2c8f1fcbffda50ac3
sahid jamal <sahidjamal151@gmail.com>
2022-10-27 14:56:38 +0530

    react redux basic

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>increament and decrement</h1>
+
+<div className='container' >
+<input type="text" value="0" className='inp' />
+<button className='a'>-</button>
+<button className='a'>+</button>
+</div>
     </div>
   );
 }

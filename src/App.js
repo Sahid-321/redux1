d4e64dc5979764addf526635fdb77b486ef3622c
sahid jamal <sahidjamal151@gmail.com>
2022-10-28 22:58:40 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useSelector, useDispatch} from "react-redux";
+import {incNumber,decNumber} from "./action/index";
 function App() {
+const myState = useSelector((state)=> state.toChange);
+const dispatch = useDispatch();
+
   return (
     <div className="App">
      <h1>increament and decrement</h1>
 
 <div className='container' >
-<input type="text" value="0" className='inp' />
-<button className='a'>-</button>
-<button className='a'>+</button>
+<input type="text" className='inp' value = {myState} />
+<button onClick={()=> dispatch(decNumber())} className='a'>-</button>
+<button onClick={()=> dispatch(incNumber())} className='a'>+</button>
 </div>
     </div>
   );
